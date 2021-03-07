8c6429c9cd4f6251c3af518ee08b965462868346
nfishe <ndfishe@gmail.com>
2021-03-06 20:39:56 -0600

    Fix missing end

--- gitlab.rb
+++ gitlab.rb
@@ -3,28 +3,31 @@ require 'set'
 require 'time'
 
 def redirected(location)
-    warn "redirected to #{location}"
-    fetch(location, limit - 1)
+  warn "redirected to #{location}"
+  fetch(location)
+end
 
-def fetch(uri_str, limit = 10)
-    response = Net::HTTP.get_response(URI(uri_str))
-    case response
-    when Net::HTTPSuccess then response
-    when Net::HTTPRedirection then redirected(response['location'])
-    else
-      response.value
-    end
+def fetch(uri_str)
+  response = Net::HTTP.get_response(URI(uri_str))
+  case response
+  when Net::HTTPSuccess
+    response
+  when Net::HTTPRedirection
+    redirected(response['location'])
+  else
+    response.value
+  end
 end
 
 if __FILE__ == $0
-    start_time = Time.now.to_i
-    current_time = start_time
-    stop_time = start_time + 5*60
+  start_time = Time.now.to_i
+  current_time = start_time
+  stop_time = start_time + 5 * 60
 
-    while current_time < stop_time
-        fetch('https://gitlab.com')
-        current_time = Time.now.to_i
+  while current_time < stop_time
+    fetch('https://gitlab.com')
+    current_time = Time.now.to_i
 
-        puts current_time - Time.now.to_i
-    end
+    puts current_time - Time.now.to_i
+  end
 end
